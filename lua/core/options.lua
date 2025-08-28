c1eb08d8739bed57bd4d78da252f0b8557cc537b
lena <lenarichards2234@gmail.com>
2025-08-28 11:45:33 +0100

    added nicer lsp diagnostic formatting

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -41,6 +41,8 @@ opt.timeoutlen = 300
 
 opt.completeopt = 'menuone,noselect'
 
+vim.diagnostic.config({ virtual_text = false, virtual_lines = {current_line = true} })
+
 opt.termguicolors = true
 
 -- Set a colored line at 80 chars
