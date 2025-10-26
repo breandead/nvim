08bb56c9b6ac519c9cfb0fd8edee99b232233eeb
lenar <lenarichards2234@gmail.com>
2025-10-26 21:10:55 +0000

    changed lsp mappings

--- lua/extern/plugins/lspconfig.lua
+++ lua/extern/plugins/lspconfig.lua
@@ -12,13 +12,13 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+        map('gr', vim.lsp.buf.rename, '[R]e[n]ame')
+        map('ga', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
         map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-        map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+        map('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('gt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
       end,
     })
 end}
