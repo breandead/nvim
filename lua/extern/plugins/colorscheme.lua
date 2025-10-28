ae87a48ff8cc58bc5442e52b502542e12fc47577
lenar <lenarichards2234@gmail.com>
2025-10-28 16:27:36 +0000

    transparent background

--- lua/extern/plugins/colorscheme.lua
+++ lua/extern/plugins/colorscheme.lua
@@ -3,6 +3,13 @@ return {
   name = "catppuccin",
   priority = 1000,
   config=function ()
+    require("catppuccin").setup({
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = true
+      }
+    })
     vim.cmd("colorscheme catppuccin")
   end
 }
